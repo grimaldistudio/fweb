4f8658b88504c0039ff2da038a3bf3fa69715f17
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-06-26 16:38:16 +0200

    commit

--- src/js/fw-init.js
+++ src/js/fw-init.js
@@ -21,5 +21,18 @@
 		// You can put your own code in here
 
 	});
+        
+        /* menu INIT */
+        var header = $(".start-style");
+		$(window).scroll(function() {    
+			var scroll = $(window).scrollTop();
+		
+			if (scroll >= 10) {
+				header.removeClass('start-style').addClass("scroll-on");
+			} else {
+				header.removeClass("scroll-on").addClass('start-style');
+			}
+		});
 
 }(jQuery));
+
