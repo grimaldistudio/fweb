08c0a029baf41cc05ea20996f5ab6567b642e882
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-06-26 16:32:04 +0200

    commit

--- header.php
+++ header.php
@@ -17,23 +17,25 @@
         </div>
     </div>
 
+<div class="navigation-wrap bg-light start-header start-style">
 <nav id="navbar" class="navbar navbar-expand-md navbar-light bg-light">
   <div class="container-fluid">
 
     <?php b4st_navbar_brand();?>
+      
 
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarDropdown" aria-controls="navbarDropdown" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+   	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+	</button>
 
-    <div class="collapse navbar-collapse" id="navbarDropdown">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <?php
         wp_nav_menu( array(
           'theme_location'  => 'navbar',
           'container'       => false,
           'menu_class'      => '',
           'fallback_cb'     => '__return_false',
-          'items_wrap'      => '<ul id="%1$s" class="navbar-nav ml-auto mt-2 mt-lg-0 %2$s">%3$s</ul>',
+          'items_wrap'      => '<ul id="%1$s" class="navbar-nav ml-auto py-4 py-md-0 mt-2 mt-lg-0 %2$s">%3$s</ul>',
           'depth'           => 2,
           'walker'          => new b4st_walker_nav_menu()
         ) );
@@ -44,5 +46,5 @@
 
   </div>
 </nav>
-
+</div>
 <?php b4st_navbar_after();?>
