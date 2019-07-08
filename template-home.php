e7ecf57d49075c502e98ac8d92810d9304e5c0fe
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-07-08 12:50:27 +0200

    tracking

--- template-home.php
+++ template-home.php
@@ -4,22 +4,27 @@
     b4st_main_before();
 ?>
 
-<main id="main" class="container mt-5">
-  <div class="row">
+<main id="main" class="mt-5">
+  
       
      <?php putRevSlider('homeslider', 'homepage'); ?>
 
-    <div class="col-sm">
+   
       <div id="content" role="main">
         
-          <div data-liindex="0" data-liref="rs-1" class="tp-tab selected" style="width: 100px; height: 50px; left: 0px; top: 0px;" onclick='revapi1.revshowslide(2);'><div class="tp-tab-content">  <span class="tp-tab-date"></span>  <span class="tp-tab-title">UNO</span></div><div class="tp-tab-image" style="background-image: url(&quot;&quot;);"></div></div>
-          
+          <!-- onclick='revapi1.revshowslide(2);' -->
         
-          
-      </div><!-- /#content -->
+          <div class="container"><!-- slider navigation and intro -->
+              <div class="row">
+                <div class="col-md-12">
+
+                </div>
+              </div>
+          </div>
+                      
     </div>
 
-  </div><!-- /.row -->
+
 </main><!-- /.container -->
 
 <?php 
