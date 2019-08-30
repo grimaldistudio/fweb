fd3aa43082166d82f088a2fdc9726cc0fdd72182
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-08-30 11:36:33 +0200

    commit

--- loops/page-content.php
+++ loops/page-content.php
@@ -6,11 +6,15 @@
 
 <?php if(have_posts()): while(have_posts()): the_post(); ?>
   <article role="article" id="post_<?php the_ID()?>" <?php post_class()?>>
+   <?php
+    /*   
     <header class="mb-5">
       <h1>
         <?php the_title()?>
       </h1>
     </header>
+     * */     
+    ?>  
     <section>
       <?php the_content()?>
       <?php wp_link_pages(); ?>
