3311b777f339c353203a2c88e05079f6560850cb
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-08-30 11:31:27 +0200

    commit

--- template-home.php
+++ template-home.php
@@ -11,16 +11,7 @@
 
    
       <div id="content" role="main">
-        
-          <!-- onclick='revapi1.revshowslide(2);' -->
-        
-          <div class="container"><!-- slider navigation and intro -->
-              <div class="row">
-                <div class="col-md-12">
-
-                </div>
-              </div>
-          </div>
+                 
           
            <div class="container">
            <?php get_template_part('loops/page-content'); ?>
