40577348594ab5ac9caa8c12e517ac10a2cf5bad
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-09-17 16:07:27 +0200

    commit

--- footer.php
+++ footer.php
@@ -23,16 +23,23 @@
 <div class="bg-black">
 <div class="container">
     <div class="row">
-        <div class="col-md-12">
-            <span class="">Seguici anche su</span>
-            <ul class="list-inline social-links">
-                <li class="list-inline-item"><a href="https://www.facebook.com/flottaweb/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
-                <li class="list-inline-item"><a href="https://twitter.com/flottaweb" target="_blank"><i class="fab fa-twitter"></i></a></li>   
-                <li class="list-inline-item"><a href="https://www.linkedin.com/company/flottaweb"><i class="fab fa-linkedin-in"></i></a></li>    
-                <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCrIfQzcQS-a69jtL4TD0tJw "><i class="fab fa-youtube"></i></a></li>   
-               
-            </ul>
+        <div class="col-md-4">
+            <img src="/wp-content/uploads/2019/09/flottaweb-w-logo.png" width="80" />
+            <span class="text-sm">Flottaweb ® è un marchio di Sistemisoft Srl. P.IVA: 02488760600. © <?php date ("Y"); ?> All Rights Reserved. </span>
+            Terms of Use and Privacy Policy
         </div>
+      
+          <div class="col-md-4">
+             <span class="">Seguici anche su</span>
+                <ul class="list-inline social-links">
+                    <li class="list-inline-item"><a href="https://www.facebook.com/flottaweb/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                    <li class="list-inline-item"><a href="https://twitter.com/flottaweb" target="_blank"><i class="fab fa-twitter"></i></a></li>   
+                    <li class="list-inline-item"><a href="https://www.linkedin.com/company/flottaweb"><i class="fab fa-linkedin-in"></i></a></li>    
+                    <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCrIfQzcQS-a69jtL4TD0tJw "><i class="fab fa-youtube"></i></a></li>   
+
+                </ul>
+        </div>
+        
     </div>
 </div>
 </div>
