6f04c9fa8307d6b499e82aa1b31e40f4dbb65a2b
Domenico Grimaldi <domenico@grimaldistudio.com>
2019-09-17 15:34:34 +0200

    commit

--- footer.php
+++ footer.php
@@ -18,6 +18,23 @@
 
 <?php b4st_bottomline();?>
 
-<?php wp_footer(); ?>
+<?php //wp_footer(); ?>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <span class="">Seguici anche su</span>
+            <ul class="list-inline social-links">
+                <li class="list-inline-item"><a href="https://www.facebook.com/flottaweb/" target="_blank"><i class="fab fa-facebook-f"></i></a></li>
+                <li class="list-inline-item"><a href="https://twitter.com/flottaweb" target="_blank"><i class="fab fa-twitter"></i></a></li>   
+                <li class="list-inline-item"><a href="https://www.linkedin.com/company/flottaweb"><i class="fab fa-linkedin-in"></i></a></li>    
+                <li class="list-inline-item"><a href="https://www.youtube.com/channel/UCrIfQzcQS-a69jtL4TD0tJw "><i class="fab fa-youtube"></i></a></li>   
+               
+            </ul>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
